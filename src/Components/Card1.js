e8bf3c210980fa87a5c3ff13e27ae5634d1ceb59
mahmoudabdo-gh <109750792+mahmoudabdo-gh@users.noreply.github.com>
2023-09-15 21:43:20 +0300

    React

---
+++ src/Components/Card1.js
@@ -0,0 +1,14 @@
+function Card1() {
+    return (
+        <>
+            <div className="container1 shadow">
+                <span className="icon1">
+                    <i className="fa-solid fa-plus fa-2xl"></i>
+                </span>
+                <h2>Writ New Card</h2>
+                <p>Your enter product design workflow - connected</p>
+            </div>
+        </>
+    );
+}
+export default Card1;
---
+++ src/Components/Card3.js
@@ -0,0 +1,16 @@
+function Card3() {
+    return (
+        <>
+            <div className="container3">
+                <h4>Download master - WestByte</h4>
+                <div>
+                    <progress value="75" max="100"> 32% </progress>
+                    <span>
+                        <strong>75%</strong>
+                    </span>
+                </div>
+            </div>
+        </>
+    );
+}
+export default Card3;
